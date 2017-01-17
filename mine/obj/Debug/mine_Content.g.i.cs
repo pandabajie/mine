d72d5ecdaad0f8b32fd94873a9ee8a5c18f3e63f
jxiong <xiongjianbang@126.com>
2017-01-17 16:56:32 +0800

    publish

--- mine/obj/Debug/mine_Content.g.i.cs
+++ mine/obj/Debug/mine_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bomb.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("timg.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bomb_32x32.ico")]
 
 
